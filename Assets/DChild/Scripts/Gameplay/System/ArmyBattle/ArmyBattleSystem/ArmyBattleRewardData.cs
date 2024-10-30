c26245779249f57fa4f5a74d65dfcfed556716fd
DESKTOP-PLSDCOA\Windows 10 <tubongbanuaallemar@gmail.com>
2024-10-30 14:38:53 +0800

    []added a missing ArmyBattleRewardsData

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSystem/ArmyBattleRewardData.cs
@@ -0,0 +1,15 @@
+using DChild.Gameplay.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    [CreateAssetMenu(fileName = "Reward", menuName = "DChild/Gameplay/Army/BattleRewards")]
+    public class ArmyBattleRewardData : ScriptableObject
+    {
+        public List<ItemData> m_Items;
+        public int m_SoulEssence;
+    }
+}
