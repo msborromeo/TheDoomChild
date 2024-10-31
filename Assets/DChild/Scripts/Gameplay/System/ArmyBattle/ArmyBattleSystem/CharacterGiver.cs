7a2074d6804316ca7801af1ef2777bf05d3b141a
DESKTOP-PLSDCOA\Windows 10 <tubongbanuaallemar@gmail.com>
2024-10-31 15:38:10 +0800

    []Added reward test Asset []Added army batte CharacterGiver

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSystem/CharacterGiver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class CharacterGiver : MonoBehaviour
+    {
+        
+    }
+}
