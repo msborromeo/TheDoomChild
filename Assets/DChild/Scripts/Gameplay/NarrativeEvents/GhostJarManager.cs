c6cb563d8f7d9ca90ae8d7c7307ef7d8853ec97b
MSI\Work <betaplayer12@gmail.com>
2024-11-04 15:47:41 +0800

    commit ghostjar

---
+++ Assets/DChild/Scripts/Gameplay/NarrativeEvents/GhostJarManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostJarManager : MonoBehaviour
+{
+    [SerializeField]
+    private int m_destroyedCounter;
+    [SerializeField]
+    private int m_totalDestroyed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
