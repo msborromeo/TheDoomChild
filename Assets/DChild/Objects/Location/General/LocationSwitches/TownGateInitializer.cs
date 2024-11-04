c414e04b5b4c53f61d046b321b71aa30a9ed8b5d
DESKTOP-PLSDCOA\Windows 10 <tubongbanuaallemar@gmail.com>
2024-11-04 15:05:41 +0800

    []Added TownGateInitializer

---
+++ Assets/DChild/Objects/Location/General/LocationSwitches/TownGateInitializer.cs
@@ -0,0 +1,33 @@
+using DChild.Gameplay.FastTravel;
+using DChild.Gameplay.Systems;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownGateInitializer : MonoBehaviour
+{
+    [SerializeField, TabGroup("Reference")]
+    private LocationSwitcher m_switcher;
+    [SerializeField, TabGroup("Reference")]
+    private LocationPoster m_Poster;
+    public FastTravelHandle fastTravel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button]
+    public void CallUI()
+    {
+        Debug.Log("Test");
+    }
+}
