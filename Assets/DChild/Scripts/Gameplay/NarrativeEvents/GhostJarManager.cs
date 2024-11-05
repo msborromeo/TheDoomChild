805500400b3784c5a38f877a4c582c92af27c212
MSI\Work <betaplayer12@gmail.com>
2024-11-05 15:22:42 +0800

    commit ghostjar

--- Assets/DChild/Scripts/Gameplay/NarrativeEvents/GhostJarManager.cs
+++ Assets/DChild/Scripts/Gameplay/NarrativeEvents/GhostJarManager.cs
@@ -1,6 +1,10 @@
+using DChild.Gameplay.Combat;
+using Holysoft.Event;
+using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GhostJarManager : MonoBehaviour
 {
@@ -8,15 +12,28 @@ public class GhostJarManager : MonoBehaviour
     private int m_destroyedCounter;
     [SerializeField]
     private int m_totalDestroyed;
+    [SerializeField]
+    private Damageable m_entity;
+    [SerializeField]
+    private UnityEvent m_afterfinaljar;
+    [SerializeField, VariablePopup(true)]
+    private string m_jarCounterDatabaseVariable;
     // Start is called before the first frame update
+    private void OnEntityDestroyed(object sender, EventActionArgs eventArgs)
+    {
+        m_destroyedCounter++;
+        DialogueLua.SetVariable(m_jarCounterDatabaseVariable, m_destroyedCounter);
+        if (m_destroyedCounter == m_totalDestroyed)
+        {
+            m_afterfinaljar?.Invoke();
+        }
+
+    }
     void Start()
     {
-        
+        m_entity.Destroyed += OnEntityDestroyed;
+        m_destroyedCounter = DialogueLua.GetVariable(m_jarCounterDatabaseVariable).asInt;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
