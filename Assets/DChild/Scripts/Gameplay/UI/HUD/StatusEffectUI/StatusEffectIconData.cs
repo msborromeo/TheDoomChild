08053fcb7a2bf25aca311a129d13c5cedfd0fdc4
JJ Besa <hybrev300@gmail.com>
2024-11-08 18:02:46 +0800

    [*] populated asset data script [*] added icon data for status effect asset data

--- Assets/DChild/Scripts/Gameplay/UI/HUD/StatusEffectUI/StatusEffectIconData.cs
+++ Assets/DChild/Scripts/Gameplay/UI/HUD/StatusEffectUI/StatusEffectIconData.cs
@@ -5,7 +5,10 @@ namespace DChild.Gameplay.Combat.StatusAilment.UI
     [CreateAssetMenu(fileName = "StatusEffectIconData", menuName = "DChild/Gameplay/Combat/Inflictions/Status Effect Icon Data")]
     public class StatusEffectIconData : ScriptableObject
     {
-
+        [SerializeField]
+        private Sprite m_activeIcon;
+        [SerializeField]
+        private Sprite m_runningDurationIcon;
     }
 
 }
