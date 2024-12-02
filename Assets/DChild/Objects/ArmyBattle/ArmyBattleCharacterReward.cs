b466c2fadf0c863eaf33fe03240f0237ff68fc34
DESKTOP-PLSDCOA\Windows 10 <tubongbanuaallemar@gmail.com>
2024-12-02 17:44:29 +0800

    []added needed components to Overworld GamplaySystem and UnderworldGameplaySystem for giving Armybattle Character to the player

---
+++ Assets/DChild/Objects/ArmyBattle/ArmyBattleCharacterReward.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.Events;
+using DChild.Gameplay.Environment.Interractables;
+using Holysoft.Event;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class ArmyBattleCharacterReward : MonoBehaviour ,IButtonToInteract
+    {
+        [SerializeField, TabGroup("Reference")]
+        private SpriteRenderer m_Graphics;
+        [SerializeField, TabGroup("Actions")]
+        private UnityEvent m_Reward;
+        [SerializeField, TabGroup("Reference")]
+        private Vector3 m_promptOffset;
+        [SerializeField, TabGroup("Character Reward")]
+        private List<ArmyCharacterData> m_CharacterReward;
+        [SerializeField]
+        private CharacterGiver m_CharacterGiver;
+
+        public event EventAction<EventActionArgs> InteractionOptionChange;
+
+        public bool showPrompt => true;
+
+        public string promptMessage => "Army Battle Character Dispenser";
+
+        public Vector3 promptPosition => transform.position + m_promptOffset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void GiveReward()
+        {
+            m_Reward?.Invoke();
+            m_CharacterGiver?.RecruitCharacter(m_CharacterReward);
+        }
+
+        public void Interact(Character character)
+        {
+            GiveReward();
+        }
+    }
+}
+
