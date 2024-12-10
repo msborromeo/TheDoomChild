96a47515056c87aaee930a32c4df4dc98f7e2dc1
DESKTOP-PLSDCOA\Windows 10 <tubongbanuaallemar@gmail.com>
2024-12-10 17:02:04 +0800

    []Added optional requirements to Army battle character recruitment []Added a special case recruiter for other special cases that is hard to reference like boss defeated/ quest finished etc.

--- Assets/DChild/Objects/ArmyBattle/ArmyBattleCharacterReward.cs
+++ Assets/DChild/Objects/ArmyBattle/ArmyBattleCharacterReward.cs
@@ -5,6 +5,9 @@ using Sirenix.OdinInspector;
 using UnityEngine.Events;
 using DChild.Gameplay.Environment.Interractables;
 using Holysoft.Event;
+using DChild.Gameplay.Items;
+using DChild.Gameplay.Trade;
+using DChild.Gameplay.Characters.Players;
 
 namespace DChild.Gameplay.ArmyBattle
 {
@@ -13,14 +16,41 @@ namespace DChild.Gameplay.ArmyBattle
         [SerializeField, TabGroup("Reference")]
         private SpriteRenderer m_Graphics;
         [SerializeField, TabGroup("Actions")]
-        private UnityEvent m_Reward;
+        private UnityEvent m_GiveReward,m_RequirementFailed;
         [SerializeField, TabGroup("Reference")]
         private Vector3 m_promptOffset;
         [SerializeField, TabGroup("Character Reward")]
         private List<ArmyCharacterData> m_CharacterReward;
-        [SerializeField]
+        [SerializeField, TabGroup("Reference")]
         private CharacterGiver m_CharacterGiver;
         [SerializeField, TabGroup("Requirements")]
+        private bool m_isFree;
+        [SerializeField, TabGroup("Requirements"), HideIf("m_isFree")]
+        private bool m_requiresSoulEssence;
+        [SerializeField, TabGroup("Requirements"), HideIf("m_isFree")]
+        private bool m_requiresItem;
+        [SerializeField, TabGroup("Requirements"), HideIf("m_isFree")]
+        private bool m_requiresCombatArt;
+        [SerializeField, TabGroup("Requirements"), HideIf("m_isFree")]
+        private bool m_requiresPrimarySkill;
+        [SerializeField, TabGroup("Requirements"), HideIf("m_isFree")]
+        private bool m_Others;
+
+        [ShowIfGroup("m_requiresSoulEssence")]
+        [SerializeField,BoxGroup("m_requiresSoulEssence/SoulEssenceRequirement")]
+        private int m_requiredSoulEssence;
+
+        [ShowIfGroup("m_requiresItem")]
+        [SerializeField, BoxGroup("m_requiresItem/ItemRequirement")]
+        private ItemData m_hasItem;
+        [SerializeField, BoxGroup("m_requiresItem/ItemRequirement")]
+        private int m_ItemAmount;
+
+        [ShowIfGroup("m_requiresPrimarySkill")]
+        [SerializeField, BoxGroup("m_requiresCombatSkill/CombatArtRequirement")]
+        private PrimarySkill m_PrimarySkill;
+
+        private bool m_RequirementAchieved;
 
 
         public event EventAction<EventActionArgs> InteractionOptionChange;
@@ -31,29 +61,61 @@ namespace DChild.Gameplay.ArmyBattle
 
         public Vector3 promptPosition => transform.position + m_promptOffset;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         public void GiveReward()
         {
-            
             m_CharacterGiver?.RecruitCharacter(m_CharacterReward);
             Debug.LogError("AHHHHHHHHHHHHHH pain");
         }
 
+        public void RequirementMet(bool isAchieved)
+        {
+            m_RequirementAchieved = isAchieved;
+        }
+
         public void Interact(Character character)
         {
-            //GiveReward();
-            m_Reward?.Invoke();
+            if(!m_isFree)
+            {
+                if(m_requiresSoulEssence)
+                {
+                    if (GameplaySystem.playerManager.player.inventory.GetCurrencyAmount(CurrencyType.SoulEssence) < m_requiredSoulEssence && m_requiredSoulEssence != 0)
+                    {
+                        m_RequirementFailed?.Invoke();
+                        return;
+                    }
+                }
+
+                if(m_requiresItem)
+                {
+                    int x = GameplaySystem.playerManager.player.inventory.GetCurrentAmount(m_hasItem);
+                    if (x == 0||x < m_ItemAmount)
+                    {
+                        m_RequirementFailed?.Invoke();
+                        return;
+                    }
+                }
+                
+                if(m_requiresPrimarySkill)
+                {
+                    if(!GameplaySystem.playerManager.player.skills.IsSkillUnlocked(m_PrimarySkill))
+                    {
+                        m_RequirementFailed?.Invoke();
+                        return;
+                    }
+                }
+
+                if(m_Others)
+                {
+                    if(!m_RequirementAchieved)
+                    {
+                        m_RequirementFailed?.Invoke();
+                        return;
+                    }
+                }
+                
+            }
+
+            m_GiveReward?.Invoke();
         }
     }
 }
