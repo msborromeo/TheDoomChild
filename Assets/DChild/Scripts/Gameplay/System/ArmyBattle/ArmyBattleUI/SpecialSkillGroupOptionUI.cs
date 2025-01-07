66f896ef88efba182dc570a086181661b8a5fab9
Hybrev <hybrev300@gmail.com>
2025-01-07 18:29:48 +0800

    [*] rework UI flow [*] special skill methods in group selection & player option scripts [*] UI placeholder upon special skill selection [*] attacking group UI script param categorization [+] more groups button special skill toggle script [+] special skill assets

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/SpecialSkillGroupOptionUI.cs
@@ -0,0 +1,37 @@
+﻿using DChild.Gameplay.ArmyBattle.SpecialSkills;
+using Doozy.Runtime.UIManager.Components;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DChild.Gameplay.ArmyBattle.UI
+{
+    public class SpecialSkillGroupOptionUI : MonoBehaviour
+    {
+              
+        [SerializeField]
+        private Sprite m_specialGlow;
+        [SerializeField]
+        private UIButton m_moreGroupsButton;
+        [SerializeField]
+        private AttackingGroupOptionUI m_attackingGroup;
+        [SerializeField]
+        private SpecialSkillNavigationToggleUI m_navigationToggle;
+
+        public void Display(ISpecialSkillGroup group)
+        {
+            m_navigationToggle.ToggleSpecialUnitNavigation(true);
+
+            m_moreGroupsButton.Id.Name = "MoreSpecialGroups";
+
+            SelectGlow(m_specialGlow);
+            m_attackingGroup.attackingPowerUI.Display(group);
+            m_attackingGroup.characterGroupUI.Display(group.GetCharacterGroup() ?? null);
+            m_attackingGroup.partyName.Display(group);
+        }
+
+        private void SelectGlow(Sprite glow)
+        {
+            m_attackingGroup.SelectGlow(glow);
+        }
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/SpecialSkillNavigationToggleUI.cs
@@ -0,0 +1,27 @@
+﻿using Doozy.Runtime.UIManager;
+using UnityEngine;
+using Doozy.Runtime.UIManager.Components;
+
+namespace DChild.Gameplay.ArmyBattle.UI
+{
+    public class SpecialSkillNavigationToggleUI : MonoBehaviour
+    {
+        [SerializeField]
+        private UIButton m_prevButton;
+        [SerializeField]
+        private UIButton m_specialPrevButton;
+        [SerializeField]
+        private UIButton m_nextButton;
+        [SerializeField]
+        private UIButton m_specialNextButton;
+
+        public void ToggleSpecialUnitNavigation(bool value)
+        {
+            m_prevButton.gameObject.SetActive(!value);
+            m_nextButton.gameObject.SetActive(!value);
+
+            m_specialPrevButton.gameObject.SetActive(value);
+            m_specialNextButton.gameObject.SetActive(value);
+        }
+    }
+}
