05e26e5c6f623ca396de517397164dea44fecad0
serpentdrake <krateusy@gmail.com>
2025-01-15 15:45:23 +0800

    [*] Created Army Group Track Availability module

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/TrackTroopAvailability.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/TrackTroopAvailability.cs
@@ -1,3 +1,5 @@
+using PixelCrushers.DialogueSystem;
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,11 +17,9 @@ namespace DChild.Gameplay.ArmyBattle.SpecialSkills.Modules
 
         [SerializeField]
         private CheckBy m_checkbyID;
-        [SerializeField]
+        [SerializeField, ShowIf("@m_checkbyID == CheckBy.TroopId")]
         private int m_id;
-        [SerializeField]
-        private int m_randomTroop;
-        [SerializeField]
+        [SerializeField, VariablePopup(true), ShowIf("@m_checkbyID == CheckBy.TrackTroopID")]
         private string m_trackTroopID;
 
         public void ApplyEffect(ArmyController owner, ArmyController target)
@@ -27,13 +27,18 @@ namespace DChild.Gameplay.ArmyBattle.SpecialSkills.Modules
             switch (m_checkbyID)
             {
                 case CheckBy.RandomTroop:
-                    var unavailableTroop = owner.controlledArmy.GetAllUnvailableGroups();
-                    var iDToPass = Random.Range(0,unavailableTroop.Count);
+                    var currentUnavailableTroops = owner.controlledArmy;
+                    var unavailableTroop =currentUnavailableTroops.GetAllUnvailableGroups();
+                    var temp = Random.Range(0,unavailableTroop.Count);
+                    var unavailableTroopID = unavailableTroop[temp].id;
 
+                    currentUnavailableTroops.SetAttackingGroupAvailability(unavailableTroopID, true);
                     break;
                 case CheckBy.TrackTroopID:
+                    owner.controlledArmy.SetAttackingGroupAvailability(DialogueLua.GetVariable(m_trackTroopID).asInt, true);
                     break;
                 case CheckBy.TroopId:
+                    owner.controlledArmy.SetAttackingGroupAvailability(m_id,true);
                     break;
             }
 
