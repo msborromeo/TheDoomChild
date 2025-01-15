25400281117f3b0ec1f71fece359dc9834e74982
Hybrev <hybrev300@gmail.com>
2025-01-15 15:49:29 +0800

    [+] elevator folder [+] morxden elevator scripts

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Interactables/Elevator/ElevatorLevelButtonUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public partial class MordenElevatorLevelSelectionUI
+{
+    public class ElevatorLevelButtonUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_leftLabel;
+        [SerializeField]
+        private TextMeshProUGUI m_rightLabel;
+
+        public void Display() { }
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/Environment/Interactables/Elevator/MordenElevatorLevelSelectionUI.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public partial class MordenElevatorLevelSelectionUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image m_sectionBackground;
+    [SerializeField]
+    private TextMeshProUGUI m_locationLabel;
+    
+
+}
