d8ac4463568f5386a63e51c9287521d6c6b08415
HolySoft_Ayan <betaplayer12@gmail.com>
2025-02-07 15:38:16 +0800

    [*] Relayout Blade info to accomodate Dynamic Changes due to Localization

---
+++ Assets/DChild/Scripts/Localization/UI/LocalizedLayoutGroup.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using I2.Loc;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using System.Collections;
+
+namespace DChild.Localization
+{
+    [RequireComponent(typeof(LayoutGroup))]
+    public class LocalizedLayoutGroup : MonoBehaviour
+    {
+        [SerializeField, ChildGameObjectsOnly]
+        private Localize[] m_toConnect;
+        private LayoutGroup m_layoutGroup;
+        private ContentSizeFitter m_contentFitter;
+
+        private void EnableLayoutGroup()
+        {
+            StopAllCoroutines();
+            StartCoroutine(QuickToggleLayoutGroup());
+        }
+
+        private void SetContentFitterActive(bool active)
+        {
+            if (m_contentFitter == null)
+                return;
+
+            m_contentFitter.enabled = active;
+        }
+
+        private IEnumerator QuickToggleLayoutGroup()
+        {
+            SetContentFitterActive(true);
+            m_layoutGroup.enabled = true;
+            yield return new WaitForSecondsRealtime(0.5f);
+            SetContentFitterActive(false);
+            m_layoutGroup.enabled = false;
+        }
+
+        private void Awake()
+        {
+            m_layoutGroup = GetComponent<LayoutGroup>();
+            m_contentFitter = GetComponent<ContentSizeFitter>();
+            foreach (var localize in m_toConnect)
+            {
+                localize.LocalizeEvent.AddListener(EnableLayoutGroup);
+            }
+            StopAllCoroutines();
+            StartCoroutine(QuickToggleLayoutGroup());
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var localize in m_toConnect)
+            {
+                localize.LocalizeEvent.RemoveListener(EnableLayoutGroup);
+            }
+        }
+    }
+}
