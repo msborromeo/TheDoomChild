9dc9aa799c3c879f6197cf5f20db1c6fe666ab4c
Merge PC <cm.taniongon@gmail.com>
2025-02-21 15:14:28 +0800

    Revert "Merge branch 'HF_#7124_PauseMenu_Event_Handle_Fix' of https://github.com/BetaPlayer12/TheDoomChild into Development"
    This reverts commit 5f93a32e4aa286367d9463ceb9fdacf8f3d0b6f8, reversing
    changes made to 8980b3813caa3adf79db8090fbf745a5e5cea05c.

--- Assets/DChild/Scripts/Gameplay/Characters/Player/Modules/WhipAttack.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Player/Modules/WhipAttack.cs
@@ -61,6 +61,7 @@ namespace DChild.Gameplay.Characters.Players.Modules
 
         public bool CanMove() => m_canMove;
         public bool CanAirWhip() => m_canAirWhip;
+        public bool IsGravityAdjusted() => m_adjustGravity;
 
         public override void Initialize(ComplexCharacterInfo info)
         {
