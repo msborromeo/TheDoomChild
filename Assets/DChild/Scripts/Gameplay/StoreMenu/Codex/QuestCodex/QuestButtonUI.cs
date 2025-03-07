94d28d8a34e093361d96b371666662d3b28f55b6
Hybrev <hybrev300@gmail.com>
2025-03-07 15:16:27 +0800

    [+] codex quest assets & folder category [+] initial UI scripts [+] codex quest UI layout [+] object prefabs for main UI

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/QuestCodex/QuestButtonUI.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace DChild.Codex.Quest.UI
+{
+    public class QuestButtonUI : MonoBehaviour
+    {
+        [SerializeField] private TypeBackgroundUI m_background;
+        [SerializeField] private QuestNameUI m_name;
+        [SerializeField] private bool m_isMainQuest;
+        [SerializeField] private bool m_isCompleted;
+
+        [Button(ButtonSizes.Large)]
+        private void Display(string questName)
+        {
+            m_background.SetBackground(m_isMainQuest);
+            m_name.Display(questName, m_isCompleted);
+        }
+    }
+}
