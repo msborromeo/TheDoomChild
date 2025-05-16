489d38b34913d0d1f7c41326ce022c0e8daba49f
serpentdrake <krateusy@gmail.com>
2025-05-16 15:14:46 +0800

    [*] Added debug button on inspector for lichlord sarcophagus

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/LichLord/LichLordSarcophagus.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/LichLord/LichLordSarcophagus.cs
@@ -51,6 +51,7 @@ public class LichLordSarcophagus : MonoBehaviour
         StartCoroutine(ExplosionRoutine());
     }
 
+    [Button]
     public void ExplosionPrep()
     {
         StartCoroutine(PreExplodeRoutine());
