d0fcfb7ff1a3caea091ce6ae8d2e10d8ce948075
Hybrev <hybrev300@gmail.com>
2025-05-21 18:02:27 +0800

    [+] dialogue signal handling script [*] dialogue interval UI signal [*] handling script prefab attachment [*] scene addition of interval signal gameobject

---
+++ Assets/DChild/Scripts/Gameplay/UI/Dialogue/DialogueSignalHandler.cs
@@ -0,0 +1,33 @@
+﻿using DarkTonic.MasterAudio.Examples;
+using DChild.Gameplay;
+using Doozy.Runtime.Signals;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+namespace DChild.UI
+{
+    public class DialogueSignalHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private SignalSender m_dialogueIntervalSignal;
+        [SerializeField]
+        private SignalSender m_dialogueEndSignal;
+
+        private static bool m_checker;
+        public void CheckSignal()
+        {
+
+            //m_conversation = DialogueManager.MasterDatabase.GetConversation(DialogueManager.lastConversationStarted);
+            //m_currentDialogue = DialogueManager.
+
+            //check if game is in cutscene
+            if (GameplaySystem.gamplayUIHandle.isInCutsceneMode)
+            {
+                m_dialogueIntervalSignal.SendSignal();
+                return;
+            }
+            
+            m_dialogueEndSignal.SendSignal();
+        }
+    }
+}
