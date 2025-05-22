12b45fdc9cc17f101f2be7c20aac007721677998
Hybrev <hybrev300@gmail.com>
2025-05-22 14:08:53 +0800

    [*] minor flow dialogue & cinema section repositions [*] signal handling script cleanup

--- Assets/DChild/Scripts/Gameplay/UI/Dialogue/DialogueSignalHandler.cs
+++ Assets/DChild/Scripts/Gameplay/UI/Dialogue/DialogueSignalHandler.cs
@@ -13,20 +13,13 @@ namespace DChild.UI
         [SerializeField]
         private SignalSender m_dialogueEndSignal;
 
-        private static bool m_checker;
         public void CheckSignal()
         {
-
-            //m_conversation = DialogueManager.MasterDatabase.GetConversation(DialogueManager.lastConversationStarted);
-            //m_currentDialogue = DialogueManager.
-
-            //check if game is in cutscene
             if (GameplaySystem.gamplayUIHandle.isInCutsceneMode)
             {
                 m_dialogueIntervalSignal.SendSignal();
                 return;
             }
-            
             m_dialogueEndSignal.SendSignal();
         }
     }
