a1f9292d7074797e329291aaa8f0302ec9e96db9
HolySoft_Ayan <betaplayer12@gmail.com>
2025-05-27 15:07:09 +0800

    [+] Refactored Grapplers into a script

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/KingPus/KingPusGrappler.cs
@@ -0,0 +1,121 @@
+﻿using Sirenix.OdinInspector;
+using Spine.Unity;
+using System.Collections;
+using UnityEngine;
+using static DChild.Gameplay.SpineAnimation;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+
+    public class KingPusGrappler : MonoBehaviour
+    {
+        [SerializeField]
+        private SpineAnimation m_animation;
+        [SerializeField, MinValue(1f)]
+        private int m_animationLayerIndex = 1;
+        [SerializeField]
+        private SkeletonUtilityBone m_boneIK;
+        [SerializeField]
+        private Transform m_overridePoint;
+        [SerializeField]
+        private SpringJoint2D m_chain;
+        [SerializeField, FoldoutGroup("Animation")]
+        private SkeletonDataAsset m_skeletonDataAsset;
+        [SerializeField, FoldoutGroup("Animation"), Spine.Unity.SpineAnimation(dataField = "m_skeletonDataAsset", startsWith = "Wall_")]
+        private string m_extendAnimation;
+        [SerializeField, FoldoutGroup("Animation"), Spine.Unity.SpineAnimation(dataField = "m_skeletonDataAsset", startsWith = "Wall_")]
+        private string m_loopAnimation;
+        [SerializeField, FoldoutGroup("Animation"), Spine.Unity.SpineAnimation(dataField = "m_skeletonDataAsset", startsWith = "Wall_")]
+        private string m_retractAnimation;
+
+        [SerializeField, ReadOnly]
+        private bool m_isExtended;
+
+        public bool isExtended => m_isExtended;
+
+#if UNITY_EDITOR
+        [Button, FoldoutGroup("Animation")]
+        private void ExtractSkeletonDataAsset()
+        {
+            m_skeletonDataAsset = m_animation.skeletonAnimation.SkeletonDataAsset;
+        }
+#endif
+        [Button, ShowIf("m_isExtended")]
+        public void Retract(float speed)
+        {
+            if (m_isExtended == false)
+                return;
+
+            StopAllCoroutines();
+            StartCoroutine(RetractRoutine(speed));
+        }
+
+        [Button, HideIf("m_isExtended")]
+        public void Extend(float speed, bool activatePhysicsAtEnd = false)
+        {
+            if (m_isExtended)
+                return;
+
+            StopAllCoroutines();
+            StartCoroutine(ExtendRoutine(speed, activatePhysicsAtEnd));
+        }
+
+        [Button]
+        public void OverrideIK(Transform target)
+        {
+            OverrideIK(target.position);
+        }
+
+        public void OverrideIK(Vector3 position)
+        {
+            m_boneIK.mode = SkeletonUtilityBone.Mode.Override;
+            m_boneIK.transform.position = position;
+            m_overridePoint.position = position;
+        }
+
+        [Button]
+        public void StopIKOverride()
+        {
+            m_boneIK.mode = SkeletonUtilityBone.Mode.Follow;
+        }
+
+        [Button]
+        public void SetPhysicsActive(bool active)
+        {
+            if (active)
+            {
+                m_chain.gameObject.SetActive(true);
+            }
+            else if (m_chain.gameObject.activeSelf)
+            {
+                m_chain.gameObject.SetActive(false);
+                m_chain.transform.localPosition = Vector3.zero;
+            }
+        }
+
+        private IEnumerator RetractRoutine(float speed)
+        {
+            var track = m_animation.SetAnimation(m_animationLayerIndex, m_retractAnimation, false);
+            track.TimeScale = speed;
+            m_animation.AddEmptyAnimation(0, 0, 0);
+            SetPhysicsActive(false);
+            yield return new WaitForSpineAnimationComplete(track);
+            m_isExtended = false;
+        }
+
+
+        private IEnumerator ExtendRoutine(float speed, bool activatePhysicsAtEnd = false)
+        {
+            var track = m_animation.SetAnimation(m_animationLayerIndex, m_extendAnimation, false);
+            track.TimeScale = speed;
+            m_animation.AddAnimation(m_animationLayerIndex, m_loopAnimation, true, 0);
+            yield return new WaitForSpineAnimationComplete(track);
+            if (activatePhysicsAtEnd)
+            {
+                SetPhysicsActive(true);
+            }
+            m_isExtended = true;
+        }
+    }
+}
+
---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/KingPus/KingPusGrapplerHandle.cs
@@ -0,0 +1,136 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class KingPusGrapplerHandle : MonoBehaviour
+    {
+        [SerializeField]
+        private KingPusGrappler[] m_grapplers;
+
+        public KingPusGrappler[] GetGrapplers() => m_grapplers;
+
+        public IEnumerator RetractRoutine(float speed)
+        {
+            for (int i = 0; i < m_grapplers.Length; i++)
+            {
+                m_grapplers[i].Retract(speed);
+            }
+
+            bool hasExtendedGrappler = true;
+            do
+            {
+                hasExtendedGrappler = false;
+                for (int i = 0; i < m_grapplers.Length; i++)
+                {
+                    if (m_grapplers[i].isExtended)
+                    {
+                        hasExtendedGrappler = true;
+                        break;
+                    }
+                }
+
+                yield return null;
+
+            } while (hasExtendedGrappler);
+        }
+
+        public IEnumerator ExtendRoutine(float speed, int tentacleCount, bool activatePhysicsAtEnd = false)
+        {
+            if (tentacleCount < 0 || tentacleCount >= m_grapplers.Length)
+                yield break;
+
+            for (int i = 0; i < tentacleCount; i++)
+            {
+                m_grapplers[i].Extend(speed, activatePhysicsAtEnd);
+            }
+
+            bool hasExtendedGrappler = true;
+            do
+            {
+                hasExtendedGrappler = false;
+                for (int i = 0; i < tentacleCount; i++)
+                {
+                    if (m_grapplers[i].isExtended)
+                    {
+                        hasExtendedGrappler = true;
+                        break;
+                    }
+                }
+
+                yield return null;
+
+            } while (hasExtendedGrappler);
+        }
+
+        public void OverrideIKs(Vector3[] positions)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                m_grapplers[i].OverrideIK(positions[i]);
+            }
+        }
+
+        [Button]
+        /// <summary>
+        /// Will Only affect Extended Grapplers
+        /// </summary>
+        /// <param name="active"></param>
+        public void SetPhysicsActive(bool active)
+        {
+            if (active)
+            {
+                foreach (var grappler in m_grapplers)
+                {
+                    if (grappler.isExtended)
+                    {
+                        grappler.SetPhysicsActive(true);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < m_grapplers.Length; i++)
+                {
+                    m_grapplers[i].SetPhysicsActive(false);
+                }
+            }
+        }
+
+        [Button]
+        public void StopIKOverrides()
+        {
+            for (int i = 0; i < m_grapplers.Length; i++)
+            {
+                m_grapplers[i].StopIKOverride();
+            }
+        }
+
+#if UNITY_EDITOR
+        [Button]
+        public void OverrideIKs(Transform[] positions)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                m_grapplers[i].OverrideIK(positions[i]);
+            }
+        }
+
+        [Button]
+        private void Retract(float speed)
+        {
+            StopAllCoroutines();
+            StartCoroutine(RetractRoutine(speed));
+        }
+
+        [Button]
+        private void Extend(float speed, int tentacleCount, bool activatePhysicsAtEnd)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ExtendRoutine(speed, tentacleCount, activatePhysicsAtEnd));
+        }
+#endif
+    }
+}
+
