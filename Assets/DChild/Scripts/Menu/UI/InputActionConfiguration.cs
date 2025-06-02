f61a9e0abc6c62e6aee0148d7934193c8cc4a3f7
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2025-06-02 19:18:43 +0800

    [+] Created InputActionConfiguration to be able to add it to data

---
+++ Assets/DChild/Scripts/Menu/UI/InputActionConfiguration.cs
@@ -0,0 +1,56 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DChild.Gameplay.UI
+{
+    [System.Serializable]
+    public class InputActionConfiguration
+    {
+        public enum InputActionType
+        {
+            NoneComposite,
+            Directional,
+            Cycle,
+            Modifier,
+            _Count
+        }
+
+        public enum DirectionActionPart
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            _Count
+        }
+
+        public enum CycleActionPart
+        {
+            Negative,
+            Positive,
+            _Count
+        }
+
+        [SerializeField]
+        private InputActionType m_actionType;
+        public InputActionType actionType => m_actionType;
+
+        //Directional
+        [SerializeField, ShowIf("@m_actionType == InputActionType.Directional")]
+        private DirectionActionPart m_directionPart;
+        public DirectionActionPart directionPart => m_directionPart;
+
+        //Cycle
+        [SerializeField, ShowIf("@m_actionType == InputActionType.Cycle")]
+        private CycleActionPart m_cycleActionPart;
+        public CycleActionPart cycleActionPart => m_cycleActionPart;
+
+        [SerializeField]
+        private InputActionReference m_inputAction;
+        public InputActionReference inputAction => m_inputAction;
+    }
+
+}
