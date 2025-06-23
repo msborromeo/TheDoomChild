004e54c9e4ca0f87cb246c28b4077f3498cfbc10
Hybrev <hybrev300@gmail.com>
2025-06-23 17:53:19 +0800

    [+] quest state UI

---
+++ Assets/DChild/Scripts/Gameplay/Quests/QuestStateUI.cs
@@ -0,0 +1,28 @@
+﻿using PixelCrushers.DialogueSystem;
+using TMPro;
+using UnityEngine;
+
+namespace DChild.Gameplay.Quests
+{
+    public class QuestStateUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI m_inProgress;
+        [SerializeField] private TextMeshProUGUI m_completed;
+
+        private bool m_isComplete;
+        public bool isComplete => m_isComplete;
+
+        public void Display(QuestState quest)
+        {
+            if (quest != QuestState.Success)
+            {
+                m_completed.gameObject.SetActive(false);
+                m_inProgress.gameObject.SetActive(true);
+
+                return;
+            }
+            m_inProgress.gameObject.SetActive(false);
+            m_completed.gameObject.SetActive(true);
+        }
+    }
+}
