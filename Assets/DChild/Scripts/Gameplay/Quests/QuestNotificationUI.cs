8bd49b83bcd713af4f4c1ea651a68fababd6dfcd
HolySoft_Ayan <betaplayer12@gmail.com>
2025-06-26 17:53:10 +0800

    [*] Connected Notification UI to Custom Fields [*] Prompt Queue is working as intended by design

--- Assets/DChild/Scripts/Gameplay/Quests/QuestNotificationUI.cs
+++ Assets/DChild/Scripts/Gameplay/Quests/QuestNotificationUI.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using PixelCrushers.DialogueSystem;
 using TMPro;
-using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using static UnityEngine.EventSystems.EventTrigger;
 
 namespace DChild.Gameplay.Quests
 {
 
     public class QuestNotificationUI : NotificationUI
     {
-        [SerializeField] private QuestLogDataList m_dataList;
-
         [SerializeField] private TextMeshProUGUI m_questTitle;
         [SerializeField] private TextMeshProUGUI m_subEntry;
         [SerializeField] private TextMeshProUGUI m_objective;
@@ -19,59 +15,32 @@ namespace DChild.Gameplay.Quests
 
         [SerializeField] private QuestStateUI m_stateUI;
 
-        /// <summary>
+        /// <summary >
         /// entryNumber will be treated as entryCount
         /// </summary>
         /// <param name="questInfo"></param>
         [Button]
         public void UpdateLog(QuestEntryArgs questInfo)
         {
-            var subEntry = QuestLog.GetQuestEntry(questInfo.questName, questInfo.entryNumber);
-            var subEntryState = QuestLog.GetQuestEntryState(questInfo.questName, questInfo.entryNumber);
-
             m_questTitle.text = questInfo.questName;
-            m_subEntry.text = subEntry;
-            m_stateUI.Display(subEntryState);
 
             if (questInfo.entryNumber > 0)
             {
-                //var quest = DialogueManager.databaseManager.masterDatabase.GetItem(questInfo.questName);
-
-                //var instructions = quest.LookupLocalizedValue($"Entry {questInfo.entryNumber} Instructions")
-                //    ?? "No objectives found.";
-
-
-                var quest = m_dataList.GetQuest(questInfo.questName);
-                var instructions = quest.LookupValue($"Entry {questInfo.entryNumber} Instructions");
-
+                var subEntry = QuestLog.GetQuestEntry(questInfo.questName, questInfo.entryNumber);
+                var subEntryState = QuestLog.GetQuestEntryState(questInfo.questName, questInfo.entryNumber);
+                m_subEntry.text = subEntry;
+                m_stateUI.Display(subEntryState);
+                var instructions = DialogueManager.MasterDatabase.GetItem(questInfo.questName).LookupValue($"Entry {questInfo.entryNumber} Description");
                 m_objective.text = instructions;
             }
+            else
+            {
+                m_subEntry.text = "";
+                m_stateUI.Display(QuestLog.GetQuestState(questInfo.questName));
 
-            //ResetEntries();
-
-            //var entryCount = questInfo.entryNumber;
-            //for (int i = 0; i < entryCount; i++)
-            //{
-            //    m_questEntries[i].text = "";
-            //    if (questInfo.entryNumber >= 0)
-            //    {
-            //        var parent = m_questEntries[i].transform.parent;
-            //        parent.gameObject.SetActive(true);
-
-            //        var entryName = QuestLog.GetQuestEntry(questInfo.questName, i + 1);
-            //        m_questEntries[i].text = FormattedText.Parse(entryName).text;
-            //    }
-            //}
+                var instructions = QuestLog.GetQuestDescription(questInfo.questName);
+                m_objective.text = instructions;
+            }
         }
-        //private void ResetEntries()
-        //{
-        //    foreach ( var entry in m_questEntries)
-        //    {
-        //        entry.text = "";  
-        //        var parent = entry.transform.parent;
-        //        parent.gameObject.SetActive(false);
-        //    }
-        //}
-
     }
 }
