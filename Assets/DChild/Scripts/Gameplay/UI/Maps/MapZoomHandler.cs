13a423e46c09b5eb57fe21afc77aa0b9a386165d
serpentdrake <krateusy@gmail.com>
2025-07-18 17:58:58 +0800

    [*] added zoom constraints [*] fast zoom scaling rate

--- Assets/DChild/Scripts/Gameplay/UI/Maps/MapZoomHandler.cs
+++ Assets/DChild/Scripts/Gameplay/UI/Maps/MapZoomHandler.cs
@@ -11,10 +11,9 @@ namespace DChild.Gameplay.UI.Map
         private RectTransform m_currentMap;
 
         private const float DEFAULT_ZOOM = 1.0f;
-        private const float MIN_ZOOM = 0.75f;
+        private const float MIN_ZOOM = 0.5f;
         private const float MAX_ZOOM = 1.5f;
 
-        private float m_currentX;
         private float m_currentY;
 
         [SerializeField] private float m_scaleRate;
@@ -22,27 +21,38 @@ namespace DChild.Gameplay.UI.Map
         public void SetupZoom(RectTransform receivedMap)
         {
             m_currentMap = receivedMap;
-            m_currentX = DEFAULT_ZOOM;
             m_currentY = DEFAULT_ZOOM;
         }
 
 
         public void Zoom()
         {
-            m_currentMap.localScale = new Vector2(m_currentX, m_currentY);
+            m_currentMap.localScale = new Vector2(m_currentY, m_currentY);
 
             var currentZoom = m_currentMap.localScale.y;
 
             var scrollWheel = Mouse.current.scroll.ReadValue();
+
+            Debug.Log($"scroll wheel y value: {scrollWheel.y}");
+            Debug.Log($"scroll wheel normalized value: {scrollWheel.normalized}");
 
             if (scrollWheel.y == 0) { return; }
 
 
             var ySign = Mathf.Sign(scrollWheel.y);
 
-            m_currentX += m_scaleRate * ySign;
             m_currentY += m_scaleRate * ySign;
 
+            if (m_currentY < MIN_ZOOM)
+            {
+                m_currentY = MIN_ZOOM;
+            }
+            else if (m_currentY > MAX_ZOOM)
+            {
+                m_currentY = MAX_ZOOM;
+            }
+
+     
         }
     }
 }
