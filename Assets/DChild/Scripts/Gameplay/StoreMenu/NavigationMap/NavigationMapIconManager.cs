0e648396756ef447e4166f2209f1ce404e113f64
Hybrev <hybrev300@gmail.com>
2025-07-25 16:01:12 +0800

    [+] POI map icon scripts [*] POI icon manager attachment to main map

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/NavigationMap/NavigationMapIconManager.cs
@@ -0,0 +1,18 @@
+﻿using Holysoft.Event;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace DChild.Gameplay.NavigationMap
+{
+    public class NavigationMapIconManager : MonoBehaviour
+    {
+        [SerializeField, AssetSelector] private PointOfInterestIconGroupUI[] m_iconGroupCollection;
+
+        public event EventAction<EventActionArgs> OnMapZoom;
+
+        public void Zoom()
+        {
+            OnMapZoom.Invoke(this, EventActionArgs.Empty);
+        }
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/NavigationMap/PointOfInterestIconGroupUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DChild.Gameplay.NavigationMap
+{
+    public class PointOfInterestIconGroupUI: MonoBehaviour
+    {
+        private RectTransform[] m_pointIcons;
+
+        public RectTransform[] pointIcons => m_pointIcons;
+
+        private void Awake()
+        {
+            m_pointIcons = GetComponentsInChildren<RectTransform>();
+        }
+    }
+}
