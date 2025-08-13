e64cace5a6801abe72817b0ed1ffe9deff9b7eb2
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2025-08-13 17:55:55 +0800

    [+] Created CurrentLocationChecker to handle checking current location [*] Added CurrentLocationChecker to Underworld Gameplay System under Overworld Teleport Handle

---
+++ Assets/DChild/Objects/LevelFinishedFeature/LevelEndFeature/CurrentLocationChecker.cs
@@ -0,0 +1,40 @@
+using DChild.Gameplay.Environment;
+using Holysoft.Collections;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DChild.Gameplay
+{
+    public class CurrentLocationChecker : SerializedMonoBehaviour
+    {
+        [SerializeField]
+        private Location m_currentLocation;
+
+        [SerializeField]
+        private Dictionary<Location, List<SceneInfo>> m_locationAndSceneRelationDictionary = new Dictionary<Location, List<SceneInfo>>();
+
+        [Button]
+        public Location CheckCurrentLocation()
+        {
+            var currentScene = SceneManager.GetActiveScene().name;
+
+            foreach(Location location in m_locationAndSceneRelationDictionary.Keys)
+            {
+                foreach(SceneInfo sceneInfo in m_locationAndSceneRelationDictionary[location])
+                {
+                    if(sceneInfo.sceneName == currentScene)
+                    {
+                        m_currentLocation = location;
+                        return m_currentLocation;
+                    }
+                }
+            }
+
+            return m_currentLocation;
+        }
+    }
+}
+
