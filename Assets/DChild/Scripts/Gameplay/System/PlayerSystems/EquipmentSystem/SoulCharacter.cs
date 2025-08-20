2bd1a1697c41eb9b91963622e79aa69a4fc2697c
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2025-08-20 19:07:21 +0800

    [+] Created Soul Slot enum [+] Created Soul Item script [+] Created Soul Character script

---
+++ Assets/DChild/Scripts/Gameplay/System/PlayerSystems/EquipmentSystem/SoulCharacter.cs
@@ -0,0 +1,20 @@
+using DChild.Gameplay.Characters.Players.SoulSkills;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.SoulSkills
+{
+    [ShowOdinSerializedPropertiesInInspector]
+    [CreateAssetMenu(fileName = "SoulCharacter", menuName = "DChild/Database/Soul Character")]
+    public class SoulCharacter : SoulSkill
+    {
+        [SerializeField]
+        private SoulSlot m_soulSlot;
+
+
+        public SoulSlot soulSlot => m_soulSlot;
+    }
+}
+
