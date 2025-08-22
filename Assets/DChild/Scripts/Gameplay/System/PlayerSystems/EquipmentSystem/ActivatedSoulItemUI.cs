4157a005745ea08a9ec20cac918afce814fb3146
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2025-08-22 13:23:50 +0800

    [+] Added scripts for Soul Item and Soul Character UI

---
+++ Assets/DChild/Scripts/Gameplay/System/PlayerSystems/EquipmentSystem/ActivatedSoulItemUI.cs
@@ -0,0 +1,39 @@
+using DChild.Gameplay.SoulSkills.UI;
+using DChild.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.SoulSkills.UI
+{
+    public class ActivatedSoulItemUI : SoulSkillButton
+    {
+        [SerializeField]
+        private SoulSlot m_soulSlot;
+
+        public override void Show(bool immidiate)
+        {
+            if (m_button == null)
+            {
+                Awake();
+            }
+            m_button.interactable = true;
+        }
+
+        public override void Hide(bool immidiate)
+        {
+            if (m_button == null)
+            {
+                Awake();
+            }
+            m_button.interactable = false;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_isAnActivatedSoulSkill = true;
+        }
+    }
+}
+
