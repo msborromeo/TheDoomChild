22460f33ecd5b0a2e8264146cdc0c005e8608f81
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2025-10-13 18:17:20 +0800

    [*] Added isDoingWhipCombo state to prevent running while whip comboing issue

--- Assets/DChild/Objects/Characters/Player/Controls/UnderworldPlayerController.cs
+++ Assets/DChild/Objects/Characters/Player/Controls/UnderworldPlayerController.cs
@@ -686,7 +686,7 @@ namespace DChild.Gameplay.Characters.Players.Modules
         #region Input Handles
         private void OnVector2PerformedInput(Vector2 vector)
         {
-            if(m_state.isChargingAttack || m_state.isDoingSwordThrust)
+            if(m_state.isChargingAttack || m_state.isDoingSwordThrust || m_state.isDoingWhipCombo)
             {
                 m_vector2Input = Vector2.zero;
                 return;
