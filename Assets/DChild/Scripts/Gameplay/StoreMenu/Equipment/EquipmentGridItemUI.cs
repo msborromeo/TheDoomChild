474035ce69f9cf61a0cb6bc0766c03fb34fe1aac
Hybrev <hybrev300@gmail.com>
2025-10-22 17:52:45 +0800

    [+] feat: equipment functionality scripts [+] feat: equipment selection UI scripts [*] feat: item selection event handling per active category [*] chore: additional sample data for functionality testing

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Equipment/EquipmentGridItemUI.cs
@@ -0,0 +1,42 @@
+using DChild.Gameplay.EquipmentSystem;
+using Holysoft.Event;
+using NSubstitute.Exceptions;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DChild.Menu.Equipment.UI
+{
+    public class EquipmentGridItemUI : MonoBehaviour
+    {
+        [SerializeField] private EquipmentSelectionUI m_selectionUI;
+        [SerializeField] private Image m_itemIcon;
+
+        private SoulEquipmentItem m_attachedItem;
+        public SoulEquipmentItem attachedItem => m_attachedItem;
+
+        public event EventAction<EventActionArgs> OnGridItemSelected;
+
+        public void Display(SoulEquipmentItem item = null)
+        {
+            m_attachedItem = item;
+
+            bool hasItem = m_attachedItem != null;
+            gameObject.SetActive(hasItem);
+
+            if (!hasItem)
+                return;
+
+            m_itemIcon.sprite = m_attachedItem.icon;
+        }
+
+        public void PrepareAttachedItem()
+        {
+            m_selectionUI.equipButtonUI.SetSelectedItem(m_attachedItem);
+            OnGridItemSelected?.Invoke(this, EventActionArgs.Empty);
+        }
+    }
+
+}
