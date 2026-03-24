b9986b5218dba1ac68e2122b9a75ec754306550a
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2026-03-24 19:47:29 +0800

    [*] Removed unnecessary reference from Slot Item Quantity Limit Color Handle [*] Applied Color Handle to Item Slot and Quick Item Slot in Inventory

--- Assets/DChild/Scripts/Gameplay/UI/InventorySlotItemQuantityLimitColorHandle.cs
+++ Assets/DChild/Scripts/Gameplay/UI/InventorySlotItemQuantityLimitColorHandle.cs
@@ -9,27 +9,15 @@ namespace DChild.Gameplay.Inventories.UI
     {
         [SerializeField]
         private ItemIndexDetailsUI m_itemIndexDetailsUI;
-        [SerializeField]
-        private UsableInventoryItemHandle m_usableItemHandle;
 
         private void OnEnable()
         {
             m_itemIndexDetailsUI.ItemDetailsDisplayed += OnItemDetailsDisplayed;
-            m_usableItemHandle.ItemConsumed += OnItemConsumed;
         }
 
         private void OnDisable()
         {
             m_itemIndexDetailsUI.ItemDetailsDisplayed -= OnItemDetailsDisplayed;
-            m_usableItemHandle.ItemConsumed -= OnItemConsumed;
-        }
-
-        private void OnItemConsumed(IStoredItem item)
-        {
-            if(item == currentItem)
-            {
-                UpdateQuantityTextColor();
-            }
         }
 
         private void OnItemDetailsDisplayed(IStoredItem item)
