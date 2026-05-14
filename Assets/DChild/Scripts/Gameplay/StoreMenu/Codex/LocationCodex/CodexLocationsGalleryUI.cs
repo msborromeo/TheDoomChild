3ef221935fe16a7d4e1077fb65f52da1c3e23e5e
JJ Besa <holysoft.ui@gmail.com>
2026-05-14 13:23:01 +0800

    [+] locations codex directory and boilerplate scripts [*] move implemented codex sections to respective directories

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/LocationCodex/CodexLocationsGalleryUI.cs
@@ -0,0 +1,27 @@
+﻿using DChild.Codex.LocationCodex;
+namespace DChild.Menu.Codex.Locations
+{
+    public class CodexLocationsGalleryUI : CodexGalleryUI<LocationCodexData, LocationCodexProgressTracker>
+    {
+        public override void SetupGalleryEntries(int page)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetupGalleryEntries()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool CheckPlayerProgress(LocationCodexData data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void RetrieveEntries()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/LocationCodex/LocationsCodexGalleryPopupInfoUI.cs
@@ -0,0 +1,12 @@
+﻿using DChild.Codex.LocationCodex;
+namespace DChild.Menu.Codex.Locations
+{
+    public class LocationsCodexGalleryPopupInfoUI : CodexGalleryPopupInfoUI<LocationCodexData>
+    {
+        protected override void UpdateInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
