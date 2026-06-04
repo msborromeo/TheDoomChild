655a918bf661dccdc51381dec26d65b600425b0d
JJ Besa <holysoft.ui@gmail.com>
2026-06-04 16:43:41 +0800

    [*] refactor name display handling

--- Assets/DChild/Scripts/Gameplay/System/Combat/UI/BossNameUI.cs
+++ Assets/DChild/Scripts/Gameplay/System/Combat/UI/BossNameUI.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace DChild.Gameplay.Combat.UI
 {
-    public class BossNameUI : MonoBehaviour , ICodexBestiaryLocalizer
+    public class BossNameUI : MonoBehaviour, ICodexBestiaryLocalizer
     {
         [SerializeField]
         private TextMeshProUGUI m_bossName;
@@ -15,28 +15,30 @@ namespace DChild.Gameplay.Combat.UI
         private TextMeshProUGUI m_bossTitle;
         [SerializeField]
         private TextMeshProUGUI m_bossNameOnly;
-        
+
         [SerializeField] private TextMeshProUGUI m_baybayinName;
 
         public event Action<BestiaryData> localizeBestiaryData;
 
         public void SetName(Boss boss)
         {
-            m_baybayinName.text = boss.creatureName;
+            m_bossNameOnly.text = boss.creatureName;
+            m_baybayinName.text = boss.creatureTitle != string.Empty ? boss.creatureTitle : boss.creatureName;
 
-            var hasTitle = boss.creatureTitle != string.Empty || boss.creatureTitle != "";
-            if (hasTitle)
-            {
-                m_bossName.text = boss.creatureName;
-                m_bossTitle.text = boss.creatureTitle;
-            }
-            else
-            {
-                m_bossNameOnly.text = boss.creatureName;
-            }
-            m_bossName.enabled = hasTitle;
-            m_bossTitle.enabled = hasTitle;
-            m_bossNameOnly.enabled = !hasTitle;
+            //var hasTitle = boss.creatureTitle != string.Empty || boss.creatureTitle != "";
+            //if (hasTitle)
+            //{
+            //    m_bossName.text = boss.creatureName;
+            //    m_bossTitle.text = boss.creatureTitle;
+            //}
+            //else
+            //{
+            //    m_bossNameOnly.text = boss.creatureName;
+            //}
+            //m_bossName.enabled = hasTitle;
+            //m_bossTitle.enabled = hasTitle;
+            //m_bossNameOnly.enabled = !hasTitle;
+
             localizeBestiaryData?.Invoke(boss.GetBestiaryData());
         }
     }
