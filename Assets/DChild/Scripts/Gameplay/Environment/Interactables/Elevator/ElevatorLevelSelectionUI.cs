7db552219ef8b21937821d43cc0512b1154e6167
Hybrev <hybrev300@gmail.com>
2026-07-10 20:43:40 +0800

    [+]  updated MD elevator UI assets [*] comment deprecated pillar adjustment logic [*] placeholder section images for functionality implementation [*] overall MD elevator UI layout update

--- Assets/DChild/Scripts/Gameplay/Environment/Interactables/Elevator/ElevatorLevelSelectionUI.cs
+++ Assets/DChild/Scripts/Gameplay/Environment/Interactables/Elevator/ElevatorLevelSelectionUI.cs
@@ -14,7 +14,7 @@ namespace DChild.Scripts.Gameplay.Environment.Interactables.Elevator
         [SerializeField] private TextMeshProUGUI m_locationLabel;
         [SerializeField] private LocationHighlighterUI m_locationHighlight;
         [SerializeField] private List<ElevatorLevelButtonUI> m_elevatorButtons;
-        [SerializeField] private RectTransform m_pillarRect;
+        //[SerializeField] private RectTransform m_pillarRect;
 
         private MovingPlatform m_elevator;
 
@@ -36,14 +36,11 @@ namespace DChild.Scripts.Gameplay.Environment.Interactables.Elevator
                 case ElevatorLocation.Upper_West:
                     levelCount = 4;
                     break;
-                //case ElevatorLocation.Upper_West:
-                //    levelCount = 3;
-                //    break;
                 default:
                     levelCount = m_elevatorButtons.Count;
                     break;
             }
-            AdjustPillarHeight(levelCount);
+            //AdjustPillarHeight(levelCount);
 
             for (int i = 0; i < labels.Length; i++)
             {
@@ -61,14 +58,14 @@ namespace DChild.Scripts.Gameplay.Environment.Interactables.Elevator
         }
 
 
-        private void AdjustPillarHeight(int levels)
-        {
-            var spacing = 32;
-            var pillarWidth = m_pillarRect.sizeDelta.x;
-            var pillarHeight = ((40 * levels) + (spacing * levels)) - spacing;
+        //private void AdjustPillarHeight(int levels)
+        //{
+        //    var spacing = 32;
+        //    var pillarWidth = m_pillarRect.sizeDelta.x;
+        //    var pillarHeight = ((40 * levels) + (spacing * levels)) - spacing;
 
-            m_pillarRect.sizeDelta = new Vector2(pillarWidth, pillarHeight);
-        }
+        //    m_pillarRect.sizeDelta = new Vector2(pillarWidth, pillarHeight);
+        //}
     }
 
 
