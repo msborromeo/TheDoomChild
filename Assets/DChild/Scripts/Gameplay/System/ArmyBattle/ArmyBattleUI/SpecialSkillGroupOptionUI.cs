6e766e020334e82ba14519b95b99a94a4e74b44f
Hybrev <hybrev300@gmail.com>
2026-08-13 00:46:46 +0800

    feat(army-battle): update consumed special skill selection
    - move consumed special skills below available options
    - disable consumed entries and display their used overlay
    - improve special-skill pagination and null handling
    - update the post-skill UI flow
    - guard dialogue callbacks when debugging Army Battle standalone

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/SpecialSkillGroupOptionUI.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/SpecialSkillGroupOptionUI.cs
@@ -17,9 +17,48 @@ namespace DChild.Gameplay.ArmyBattle.UI
         private Image m_icon;
         [SerializeField]
         private Sprite m_specialGlow;
+        [SerializeField]
+        private UIButton m_armyRow;
+        [SerializeField]
+        private GameObject m_usedOverlay;
 
         private ISpecialSkillGroup m_group;
+        private bool m_isUsed;
         public ISpecialSkillGroup group => m_group;
+        public bool isUsed => m_isUsed;
+
+        public void SetUsed(bool isUsed)
+        {
+            m_isUsed = isUsed;
+
+            if (m_armyRow == null)
+            {
+                m_armyRow = GetComponent<UIButton>();
+            }
+
+            if (m_usedOverlay == null)
+            {
+                var childTransforms = GetComponentsInChildren<Transform>(true);
+                for (int i = 0; i < childTransforms.Length; i++)
+                {
+                    if (childTransforms[i].name == "Image - UsedGroupOverlay")
+                    {
+                        m_usedOverlay = childTransforms[i].gameObject;
+                        break;
+                    }
+                }
+            }
+
+            if (m_usedOverlay != null)
+            {
+                m_usedOverlay.SetActive(isUsed);
+            }
+
+            if (m_armyRow != null)
+            {
+                m_armyRow.interactable = !isUsed;
+            }
+        }
 
         public void Display(ISpecialSkillGroup group)
         {
@@ -40,6 +79,8 @@ namespace DChild.Gameplay.ArmyBattle.UI
                 m_icon.color = m_icon.sprite ? Color.white : Color.clear;
                 return;
             }
+
+            m_isUsed = false;
             gameObject.SetActive(false);
         }
     }
